e8145373a614bda2cfdbf65908b4c25f0e09ef2c
joezerSmaniotto <joezersmaniotto5@gmail.com>
2023-07-06 06:31:54 -0300

    Criação do Header e aplicação do de Icons

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,15 @@
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
 import { Container } from './styles';
 
 export function Groups() {
   return (
     <Container>
-      
+      <Header showBackButton />
+      <Highlight 
+        title="Turmas"
+        subtitle="Jogue com sua turma"
+      />
     </Container>
   );
 }
