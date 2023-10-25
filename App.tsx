59e24b9fa861b8260deed50b6f0f13b6b6dd05de
joezerSmaniotto <joezersmaniotto5@gmail.com>
2023-10-24 21:10:04 -0300

    create sreen group and components

--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
 import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
-import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import theme from './src/theme'
 
 import { Groups } from '@screens/Groups';
-import {Loading} from '@components/Loading';
+import { Loading } from '@components/Loading';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
   /*Na linha acima neste possição do array tenho um propriedade que me dirá
   se foi carregado as fonts ou não, neste caso, NESTE CASO defino o nome 
   fontsLoaded, para saber se foi carregado, caso não tenha sido, 
@@ -22,8 +22,8 @@ export default function App() {
         backgroundColor="transparent" // Quando aplico barStyle, ele aplica um backgorund preto, então por isso coloco aqui o background transparent
         translucent // Com o transluci minha interface que começa baixo da barra de status, a partir de começa no TOPO da tela
       />
-      {fontsLoaded ? <Groups/> : <Loading/>}
-    </ThemeProvider>  
+      {fontsLoaded ? <Groups /> : <Loading />}
+    </ThemeProvider>
   );
 }
 
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,36 @@
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
 import { Container } from './styles';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 
 export function Groups() {
+
+  const [groups, setGroups] = useState<string[]>([])
   return (
     <Container>
       <Header showBackButton />
-      <Highlight 
+      <Highlight
         title="Turmas"
         subtitle="Jogue com sua turma"
+      />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={(
+          <ListEmpty
+            message="Que tal cadastrar a primeira turma?"
+          />
+        )}
       />
     </Container>
   );
