2e097507b6e15a6995eb9560e892644e351556f8
joezerSmaniotto <joezersmaniotto5@gmail.com>
2023-10-27 08:57:27 -0300

    rename NumberOfPlayers

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -10,7 +10,7 @@ import { Button } from "@components/Button"
 import { PlayerCard } from "@components/PlayerCard";
 import { ListEmpty } from "@components/ListEmpty";
 
-import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 
 export function Players() {
   const [teams, setTeams] = useState('Time A');
@@ -45,9 +45,9 @@ export function Players() {
           horizontal
         />
 
-        <NumbersOfPlayers>
+        <NumberOfPlayers>
           {players.length}
-        </NumbersOfPlayers>
+        </NumberOfPlayers>
       </HeaderList>
 
       <FlatList
